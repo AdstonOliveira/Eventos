91f5bfc8d6b7cbf1ddbad6a8311e9b055944a691
Lais Rebello Tavares <rebello.tavares@gmail.com>
2019-10-30 16:53:19 -0300

    criação das migrations

---
+++ database/migrations/2019_10_30_194718_participantes.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Participantes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('participantes', function (Blueprint $table)
+	 {
+           		$table->increments('id');
+                $table->string('nome', 100);
+                $table->string('rg', );
+                $table->string('cpf', 11);
+                $table->string('email', 80);
+                $table->string('telefone', 80);
+                $table->date('data_nascimento', 80);
+                $table->string('organizacao', 80);
+                $table->timestamps();
+        	});        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
