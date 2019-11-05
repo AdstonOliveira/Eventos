ae195c839e8217b259e7d8bdba6ff4b5c6514de4
DeniseLopes <denise@kosloski.com.br>
2019-11-05 16:07:38 -0300

    destroy, restore

--- resources/views/participante/index.blade.php
+++ resources/views/participante/index.blade.php
@@ -5,9 +5,9 @@
     <div class="row">
 
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-header text-center"><b>Relações de Participantes</b></div>
-                <div class="col-md-12" style= "padding: 20px;">
+            <div class="card border-success mb-3">
+                <div class="card-header text-center text-success"><b>Relações de Participantes</b></div>
+                <div class="col-md-12" style="padding: 20px;">
                     <div class="text-right">
                         <a href="{{url('participante/create')}}" class="btn btn-success">Novo Participante</a>
                     </div>
@@ -26,7 +26,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($data['participantes'] as $participante)
+                        @foreach($participantes as $participante)
                         <tr>
                             <td>{{$participante->nome}}</td>
                             <td>{{$participante->rg}}</td>
@@ -45,6 +45,52 @@
                                         <input type="submit" class="btn btn-danger" value="Desativar" />
                                     </form>
                                 </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--Produto Inativo -->
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-12">
+        <div class="card border-danger mb-3" >
+                <div class="card-header text-center text-danger"><b>Participantes Inativos</b></div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>RG</th>
+                            <th>CPF</th>
+                            <th>Email</th>
+                            <th>Telefone</th>
+                            <th>Data de Nascimento</th>
+                            <th>Organização</th>
+                            <th colspan='3'>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($participantesInativos as $participante)
+                        <tr>
+                            <td>{{$participante->nome}}</td>
+                            <td>{{$participante->rg}}</td>
+                            <td>{{$participante->cpf}}</td>
+                            <td>{{$participante->email}}</td>
+                            <td>{{$participante->telefone}}</td>
+                            <td>{{$participante->data_nascimento}}</td>
+                            <td>{{$participante->organizacao}}</td>
+                            <td>
+                                <form action="{{url('participante', [$participante->id])}}" method="POST">
+                                    {{method_field('DELETE')}}
+                                    {{ csrf_field() }}
+                                    <input type="submit" class="btn btn-danger" value="Ativar" />
+                                </form>
                             </td>
                         </tr>
                         @endforeach
